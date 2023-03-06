98ff86c8e4adea2785abcbaae35a5583dea0bc94
NICOLLAS PADILHA DE SOUZA <nicollas.3901@aluno.pr.senac.br>
2023-03-06 08:46:19 -0300

    PHP

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    $nome="Nicollas";
+    $idade="15 Anos";
+    $endereco="Rua Augostinho Cremasco, 36 - Jardim Aeroporto";
+    $cidade="Ivaiporã";
+    $cep="86.870-000";
+    $primeirosemestre="8";
+    $segundosemestre="10";
+    $terceirosemestre="9";
+    $media=($primeirosemestre+$segundosemestre+$terceirosemestre)/3;
+
+    echo("Seu nome é: ") . $nome;
+    echo ("</br>");
+    echo ("Sua idade é: ") . $idade;
+    echo ("</br>");
+    echo ("Seu endereço é: ") . $endereco;
+    echo ("</br>");
+    echo ("Sua cidade é: ") . $cidade;
+    echo ("</br>");
+    echo ("Seu cep é: ") . $cep;
+    echo ("</br>");
+    echo ("Seu primeiro semestre é: ") . $primeirosemestre;
+    echo ("</br>");
+    echo ("Seu segundo semestre é: ") . $segundosemestre;
+    echo ("</br>");
+    echo ("Seu terceiro semestre é: ") . $terceirosemestre;
+    echo ("</br>");
+    echo ("Sua média é: ") . $media
+?>
